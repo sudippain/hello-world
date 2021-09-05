1305705ffd9fdd5384f0a653f3496117d6cface3
Sudip Pain <swastidip96@gmail.com>
2021-09-05 17:07:23 +0530

    Update TestGreeter.java

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -19,7 +19,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeTheOneBeingGreeted() {
-    String someone = "World1";
+    String someone = "";
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
